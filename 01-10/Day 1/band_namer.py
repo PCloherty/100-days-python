69d96ec910762a6e1aa14f964c231dcd05d75729
PCloherty <p.cloherty@hotmail.co.uk>
2024-05-09 15:27:43 +0100

    initial commit with day 1

---
+++ 01-10/Day 1/band_namer.py
@@ -0,0 +1,9 @@
+#welcome prompt
+print("So you want a band name?")
+#colour variable
+colour = input("First what is your favorite colour?\n")
+#animal variable
+animal = input("Secondly what is your favorite animal?\n")
+
+#combine colour variable with animal variable
+print("Your band name is " + colour + " " + animal)
